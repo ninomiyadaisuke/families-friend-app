ffbd2bb05f8ccb714cf4f713ad9b242c764c28cd
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-18 14:31:55 +0900

    create ResponsiveImage component

---
+++ src/components/elements/images/ResponsiveImage.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import { FC } from 'react';
+
+import styles from '@/styles/components/elements/images/responsiveImage.module.scss';
+
+type Props = {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+};
+
+const ResponsiveImage: FC<Props> = (props) => {
+  const { src, alt, width, height } = props;
+  return <Image src={src} alt={alt} width={width} height={height} sizes="100vw" className={styles.image} />;
+};
+
+export default ResponsiveImage;
