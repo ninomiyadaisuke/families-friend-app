211e0b00d9d261d8bd415a569b21ba31fded8d31
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-19 15:26:23 +0900

    Added animation using Framer motion.

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { FC } from 'react';
 
 import { ResponsiveImage } from '@/components/elements/images';
@@ -6,7 +7,9 @@ import styles from '@/styles/components/home/hero.module.scss';
 const Hero: FC = () => {
   return (
     <section className={styles.hero}>
-      <ResponsiveImage src="/home/hero-image.svg" alt="hero-image" width={1500} height={800} />
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+        <ResponsiveImage src="/home/hero-image.svg" alt="hero-image" width={1500} height={800} />
+      </motion.div>
     </section>
   );
 };
