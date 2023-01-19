db6f79d0877eac9cfbbdd90ae6bae9e89f2112a6
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-19 11:42:25 +0900

    create Footer component

---
+++ src/styles/components/elements/footer/footer.module.scss.d.ts
@@ -0,0 +1,17 @@
+export type Styles = {
+  footer: string;
+  footer__container: string;
+  footer__icons: string;
+  footer__icons_facebook: string;
+  footer__icons_instagram: string;
+  footer__icons_twitter: string;
+  footer__links: string;
+  footer__logoArea: string;
+  footer__logoArea_logo: string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
