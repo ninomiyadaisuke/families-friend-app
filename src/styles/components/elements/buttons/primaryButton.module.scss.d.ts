c6367935c634db0b9b99595bf6d3e8d823381c00
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-31 13:19:44 +0900

    create PrimaryButton

---
+++ src/styles/components/elements/buttons/primaryButton.module.scss.d.ts
@@ -0,0 +1,12 @@
+export type Styles = {
+  button__light_blue: string;
+  button__navy: string;
+  button__outline: string;
+  button__wine_red: string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
