51dd20a18e5863094368f66b207c19e2ba991a4f
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-07 15:33:08 +0900

    create AuthLayout component

---
+++ src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,32 @@
+import { FC, ReactNode } from 'react';
+
+import { ResponsiveImage } from '@/components/elements/images';
+import { useBreakPoint } from '@/hooks/useBreakPoint';
+import styles from '@/styles/components/layouts/authLayout.module.scss';
+
+type Props = {
+  children: ReactNode;
+};
+
+const AuthLayout: FC<Props> = (props) => {
+  const { children } = props;
+  const { tablet } = useBreakPoint();
+  return (
+    <div className={styles.container}>
+      {!tablet && <aside className={styles.aside} />}
+      <section>
+        <h1 className={styles.title}>
+          <ResponsiveImage
+            src={tablet ? '/logo/auth-logo-sp.svg' : '/logo/auth-logo-pc.svg'}
+            alt="auth-logo"
+            width={tablet ? 305 : 262}
+            height={tablet ? 68 : 50}
+          />
+        </h1>
+        <div className={styles.children}>{children}</div>
+      </section>
+    </div>
+  );
+};
+
+export default AuthLayout;
