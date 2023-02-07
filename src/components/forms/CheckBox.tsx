0f2f7adbde741d55335bebd67caa54b4249d5811
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-07 13:36:26 +0900

    fix CheckBox component

--- src/components/forms/CheckBox.tsx
+++ src/components/forms/CheckBox.tsx
@@ -4,14 +4,22 @@ import type { UseFormRegisterReturn } from 'react-hook-form';
 import styles from '@/styles/components/forms/checkbox.module.scss';
 
 type Props = {
-  registration: UseFormRegisterReturn;
+  registration?: UseFormRegisterReturn;
 };
 
 const CheckBox: FC<Props> = (props) => {
   const { registration } = props;
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.currentTarget.click();
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className={styles.checkbox}>
-      <input id="checkbox" type="checkbox" value={'retention'} {...registration} />
+      <input id="checkbox" type="checkbox" value={'retention'} {...registration} onKeyDown={handleKeyDown} />
       <label htmlFor="checkbox"></label>
     </div>
   );
