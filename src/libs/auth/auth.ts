efbbbab8e5d4f334fc1416fd97db41169e50c275
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-27 10:48:47 +0900

    create auth.ts

---
+++ src/libs/auth/auth.ts
@@ -0,0 +1,42 @@
+import { NextApiResponse } from 'next';
+import { setCookie } from 'nookies';
+
+export const singUp = async (email: string, password: string) => {
+  const apiKey = process.env.FIREBASE_API_KEY;
+  const body = JSON.stringify({ email, password, returnSecureToken: true });
+  return await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+};
+
+export const login = async (email: string, password: string) => {
+  const apiKey = process.env.FIREBASE_API_KEY;
+  const body = JSON.stringify({ email, password });
+  return await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+};
+
+export const assignSession = async (res: NextApiResponse, idToken: string, expiresIn: number) => {
+  const SESSION_KEY = 'session';
+  const COOKIE_OPTIONS = {
+    maxAge: expiresIn,
+    httpOnly: true,
+    secure: true,
+    path: '/',
+  };
+  setCookie({ res }, SESSION_KEY, idToken, COOKIE_OPTIONS);
+};
+
+export const logout = async () => {
+  // セッションCookieを削除するため、Firebase SDKでなくREST APIでログアウトさせる
+  await fetch('/api/sessionLogout', { method: 'POST' });
+};
