591d7fedb924c7b758836267abbc90e3d111e856
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-27 11:51:36 +0900

    create sessionLogout.ts

---
+++ src/pages/api/auth/sessionLogout.ts
@@ -0,0 +1,24 @@
+import type { NextApiHandler } from 'next';
+import { destroyCookie, parseCookies } from 'nookies';
+
+import { auth } from '@/firebase/firebaseAdmin';
+
+const handler: NextApiHandler = async (req, res) => {
+  // POSTじゃなければ、"404 Not Found"を返す
+  if (req.method !== 'POST') return res.status(404).send('Not Found');
+  // Cookieに保存されているセッションIDを取得する
+  const sessionId = parseCookies({ req }).session || '';
+  // セッションIDから認証情報を取得する
+  const decodedClaims = await auth.verifySessionCookie(sessionId).catch(() => null);
+
+  // 全てのセッションを無効にする
+  if (decodedClaims) {
+    await auth.revokeRefreshTokens(decodedClaims.sub);
+  }
+
+  // Cookieに保存されているセッションIDを削除
+  destroyCookie({ res }, 'session', { path: '/' });
+  res.send(JSON.stringify({ status: 'success' }));
+};
+
+export default handler;
