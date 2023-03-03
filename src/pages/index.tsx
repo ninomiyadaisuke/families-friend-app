2789f0d747006d441e8a8998bbc7b71dfff61c67
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-03-03 14:09:15 +0900

    create DropDawnLinks component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPageWithLayout } from 'next';
 import { ReactElement } from 'react';
 
+import { DropDawnLinks } from '@/components/elements/links';
 import { Hero, HomeAbout, HomeService } from '@/components/home';
 import { Layout } from '@/components/layouts';
+
+const links = [
+  { text: 'マイページ', href: '/' },
+  { text: 'プロフィール編集', href: '/' },
+];
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
-      <Hero />
+      <div style={{ margin: '300px' }}>
+        <DropDawnLinks links={links} auth={'ログアウト'} />
+      </div>
+      {/* <Hero />
       <HomeAbout />
-      <HomeService />
+      <HomeService /> */}
     </>
   );
 };
