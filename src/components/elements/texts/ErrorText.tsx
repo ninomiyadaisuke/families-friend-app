31ac843303fc9d8315136da5e5cf613a07857985
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-03-14 10:02:58 +0900

    create ErrorText component

---
+++ src/components/elements/texts/ErrorText.tsx
@@ -0,0 +1,16 @@
+import cx from 'classnames';
+import { FC, ReactNode } from 'react';
+
+import styles from '@/styles/components/elements/texts/errorText.module.scss';
+
+type Props = {
+  children: ReactNode;
+  positon: string;
+};
+
+const ErrorText: FC<Props> = (props) => {
+  const { children, positon } = props;
+  return <p className={cx(styles.errorText, positon)}>{children}</p>;
+};
+
+export default ErrorText;
