f5e2eb070014290731db20852efa5f5fbb381e1a
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-03-15 16:23:21 +0900

    create ImageUploader component

--- src/styles/components/forms/ImageUploader.module.scss.d.ts
+++ src/styles/components/forms/ImageUploader.module.scss.d.ts
@@ -2,6 +2,13 @@ export type Styles = {
   buttonContainer: string;
   buttonContainer__input: string;
   buttonContainer__label: string;
+  deleteImage: string;
+  profileImage: string;
+  thumbnailImage: string;
+  uploaderContainer: string;
+  uploaderContainer__imageAndButton: string;
+  uploaderContainer__text: string;
+  uploaderContainer_imagePostion: string;
 };
 
 export type ClassNames = keyof Styles;
