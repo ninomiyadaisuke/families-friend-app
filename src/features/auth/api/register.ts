1c4f679cb2e93603a72767764b2addf3add43997
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-03-22 11:48:56 +0900

    save

--- src/features/auth/api/register.ts
+++ src/features/auth/api/register.ts
@@ -3,32 +3,27 @@ import Router from 'next/router';
 import { z } from 'zod';
 
 import { familiesRef, timeStamp, usersRef } from '@/firebase';
-import {
-  birthday,
-  confirmEmail,
-  confirmPassword,
-  email,
-  firstName,
-  firstNameKana,
-  lastName,
-  lastNameKana,
-  password,
-  relationship,
-} from '@/libs/validation';
+import { userSchema } from '@/firebase/schema/userSchema';
+import { confirmEmail, confirmPassword, password } from '@/libs/validation';
 
-export const registerSchema = z
-  .object({
-    password: password,
-    confirm_password: confirmPassword,
-    email: email,
-    confirm_email: confirmEmail,
-    first_name: firstName,
-    first_name_kana: firstNameKana,
-    last_name: lastName,
-    last_name_kana: lastNameKana,
-    birthday: birthday,
-    relationship: relationship,
-  })
+const userRegistrationSchema = userSchema.pick({
+  email: true,
+  first_name: true,
+  first_name_kana: true,
+  last_name: true,
+  last_name_kana: true,
+  birthday: true,
+  relationship: true,
+});
+
+const registrationValidationSchema = z.object({
+  password,
+  confirm_password: confirmPassword,
+  confirm_email: confirmEmail,
+});
+
+export const registerSchema = userRegistrationSchema
+  .merge(registrationValidationSchema)
   .superRefine(({ password, confirm_password, email, confirm_email }, ctx) => {
     if (password !== confirm_password) {
       ctx.addIssue({
