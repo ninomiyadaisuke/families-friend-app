845ebbd36438379685db5e4538e3c1af1c890e2b
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-03-27 14:50:56 +0900

    create AddButton component base

---
+++ src/components/elements/buttons/AddButton.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import styles from '@/styles/components/elements/buttons/addButton.module.scss';
+
+const AddButton: FC = () => {
+  return <a className={styles.button}>AddButton</a>;
+};
+
+export default AddButton;
