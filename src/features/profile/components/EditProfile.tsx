b422b23b8f8523e93c03ca3135147b2b0cd36480
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-04-04 16:42:00 +0900

    create EditProfile component base

---
+++ src/features/profile/components/EditProfile.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+import { AddButton, PrimaryButton } from '@/components/elements/buttons';
+import { PersonalInfo, PrimaryInput } from '@/components/forms';
+
+const EditProfile: FC = () => {
+  return <div>EditProfile</div>;
+};
+
+export default EditProfile;
