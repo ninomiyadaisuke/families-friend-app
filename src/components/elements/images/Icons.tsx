7d5127d4afef8518d3a0bf237c9b7260320fc3c1
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-05-12 14:00:21 +0900

    emailicon

--- src/components/elements/images/Icons.tsx
+++ src/components/elements/images/Icons.tsx
@@ -4,9 +4,10 @@ import { FixedImage } from '@/components/elements/images';
 import styles from '@/styles/components/elements/images/icons.module.scss';
 
 interface IconsProps {
-  TrashCanIcon: FC; // TrashCanIconの型を指定
+  TrashCanIcon: FC;
   PencilSquareIcon: FC;
   PencilIcon: FC;
+  EmailIcon: FC;
 }
 
 const Icons: IconsProps = {
@@ -14,7 +15,8 @@ const Icons: IconsProps = {
   PencilSquareIcon: () => (
     <FixedImage src="/icon/edit-icon.svg" alt="edit-icon" className={styles.icons__pencilSquare} />
   ),
-  PencilIcon: () => <FixedImage src="/public//icon/pencil-icon.svg" alt="edit-icon" className={styles.icons__pencil} />,
+  PencilIcon: () => <FixedImage src="/icon/pencil-icon.svg" alt="edit-icon" className={styles.icons__pencil} />,
+  EmailIcon: () => <FixedImage src="/icon/email-icon.svg" alt="email-icon" className={styles.icons__email} />,
 };
 
 export default Icons;
--- src/styles/components/elements/images/icons.module.scss.d.ts
+++ src/styles/components/elements/images/icons.module.scss.d.ts
@@ -1,4 +1,5 @@
 export type Styles = {
+  icons__email: string;
   icons__pencil: string;
   icons__pencilSquare: string;
   icons__trash: string;
