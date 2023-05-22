72da512905bb6a3aba63f19bf98a86212c80dad7
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-05-22 13:53:56 +0900

    create FamilyCard

--- src/components/elements/cards/FamilyCard.tsx
+++ src/components/elements/cards/FamilyCard.tsx
@@ -1,6 +1,9 @@
+import HTMLReactParser from 'html-react-parser';
+import Link from 'next/link';
 import { FC } from 'react';
 
 import { Icons, ProfileImages } from '@/components/elements/images';
+import styles from '@/styles/components/elements/cards/familyCard.module.scss';
 
 type Props = {
   name: string;
@@ -9,22 +12,37 @@ type Props = {
   address?: string;
   zipCode?: string;
   image?: string;
+  href: string;
+  onClick: () => void;
 };
 
 const FamilyCard: FC<Props> = (props) => {
-  const { name, householdSize, phoneNumber = 'null', address = 'null', zipCode = 'null', image } = props;
+  const { name, householdSize, phoneNumber = 'null', address = 'null', zipCode = 'null', image, href, onClick } = props;
+
   return (
-    <div>
-      <ProfileImages.CircleImageSecondary image={image} />
-      <h3>{`${name}家`}</h3>
-      <p>{`${householdSize}人家族`}</p>
-      <p>{`${phoneNumber}`}</p>
-      <p>{`住所： 〒${zipCode} ${address}人家族`}</p>
-      <div>
-        <Icons.PencilIcon />
+    <Link href={href} className={styles.card}>
+      <div className={styles.card__profile}>
+        <div className={styles.card__image}>
+          <ProfileImages.CircleImageSecondary image={image} />
+        </div>
+        <div className={styles.card__texts}>
+          <h3 className={styles.card__title}>{`${name}家`}</h3>
+          <p className={styles.card__text}>{`${householdSize}人家族`}</p>
+          <p className={styles.card__text}>{`${phoneNumber}`}</p>
+          <p className={styles.card__text}>{HTMLReactParser(`<span>住所：〒${zipCode}</span>${address}`)}</p>
+        </div>
+      </div>
+      <div
+        className={styles.card__icon}
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          onClick();
+        }}
+      >
         <Icons.TrashCanIcon />
       </div>
-    </div>
+    </Link>
   );
 };
 
